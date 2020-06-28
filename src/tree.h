a4c1dfe5dc5e60364d0676a3d6166cd83f3bbaba
Tiago de Oliveira Januario <januarioccp@gmail.com>
2020-06-28 12:24:01 -0300

    Main functions

--- src/tree.h
+++ src/tree.h
@@ -1,7 +1,28 @@
 #ifndef TREE_H_B_AND_P
 #define TREE_H_B_AND_P
+
+#include "node.h"
+
 #include <ilcplex/ilocplex.h>
 
+#include <list>
+#include <stack>
+#include <queue>
+#include <vector>
+using namespace std;
 
+class Tree
+{
+public:
+	Tree();
+	double deepFirstSearch();
+	double breadthFirstSearch();
+	double bestBoundSearch();
+private:
+	static double UB;
+	list<Node> bfsTree;
+	stack<Node> dfsTree;
+	priority_queue<Node, vector<Node>, CompareNode> bestTree;
+};
 
 #endif
