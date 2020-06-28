bea8f584962e4996736cc2d2d56d3401d979c0a1
Tiago de Oliveira Januario <januarioccp@gmail.com>
2020-06-28 12:28:30 -0300

    Pricing and compare structure

--- src/node.h
+++ src/node.h
@@ -1,15 +1,41 @@
 #ifndef NODE_H_B_AND_P
 #define NODE_H_B_AND_P
 
+#include "data.h"
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
 class Node
 {
 public:
 	Node();
-	double bound();
+	double bound() const;
 	void boundSet();
+	pair<int, int> pricing(Data *in);
 
 private:
+	vector<pair<int, int> > sameBin;
 	double bound_;
+};
+
+struct CompareNode
+{
+	bool operator()(Node const &p1, Node const &p2)
+	{
+		// return "true" if "p1" is before "p2":
+		return p1.bound() < p2.bound();
+		return true;
+		// else{
+
+		// 	if((p1.subtour[p1.escolhido].size() - p1.escolhido) < (p2.subtour[p2.escolhido].size() - p2.escolhido))
+		// 		return true;
+		// 	else {
+		// 		return (p1.subtour.size() < p2.subtour.size());
+		// 	}
+		// }
+	}
 };
 
 #endif
